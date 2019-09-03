1b14371712bfe773d4bfd575638c6f81ec848896
Jeffrey Clark <jclark@nmi.com>
2019-09-03 11:58:39 -0500

    PEP8 and python 3 compat, remove docopt

--- check-consul-health.py
+++ check-consul-health.py
@@ -1,88 +1,95 @@
 #!/usr/bin/python
-"""Usage: 
-    check-consul-health.py node NODE DC
-        [--addr=ADDR]
-        [--CheckID=CheckID | --ServiceName=ServiceName]
-        [--verbose]
 
-Arguments:
-    NODE  the consul node_name
-    DC    the consul datacenter
+import argparse
+import traceback
+import requests
 
-Options:
-    -h --help                  show this
-    -v --verbose               verbose output
-    --addr=ADDR                consul address [default: http://localhost:8500]
-    --CheckID=CheckID          CheckID matcher
-    --ServiceName=ServiceName  ServiceName matcher
-"""
-
-from docopt import docopt
-import requests, json, traceback, exceptions
 
 def dump(it):
-    if arguments['--verbose']: print it
+    if arguments['debug']:
+        print(it)
 
-def buildNodeUrl():
-    url = "%(--addr)s/v1/health/node/%(NODE)s?dc=%(DC)s" % arguments
-    dump("Url: " + url)
+
+def build_node_url():
+    url = "%(addr)s/v1/health/node/%(NODE)s?dc=%(DC)s" % arguments
     return url
 
-def getJsonFromUrl(url):
+
+def get_json_from_url(url):
+    dump("url: {0}".format(url))
     r = requests.get(url)
     dump("Response: " + r.text)
     dump("Status code: " + str(r.status_code))
     r.raise_for_status()
     return r.json()
 
-def printCheck(check):
-    print "> %(Node)s:%(ServiceName)s:%(Name)s:%(CheckID)s:%(Status)s" % check
 
-def processFailing(checks):
-    filters = map(lambda field: \
-        lambda x: arguments['--' + field] is None or x[field] == arguments['--'+field],
-        ['CheckID', 'ServiceName']
-    )
+def print_check(check):
+    print("> %(Node)s:%(ServiceName)s:%(Name)s:%(CheckID)s:%(Status)s" % check)
 
-    filtered = filter(lambda x: all(f(x) for f in filters), checks)
-    passing  = filter(lambda x: x['Status'] == 'passing', filtered)
-    warning  = filter(lambda x: x['Status'] == 'warning', filtered)
-    critical = filter(lambda x: x['Status'] == 'critical', filtered)
+
+def process_failing(checks):
+    def check_output(x):
+        return x["Name"] + ":" + x["Output"]
+
+    filters = map(lambda field: lambda x: arguments[field] is None or x[field] == arguments[field],
+                  ['CheckID', 'ServiceName']
+                  )
+
+    filtered = list(filter(lambda x: all(f(x) for f in filters), checks))
+    passing = list(filter(lambda x: x['Status'] == 'passing', filtered))
+    warning = list(filter(lambda x: x['Status'] == 'warning', filtered))
+    critical = list(filter(lambda x: x['Status'] == 'critical', filtered))
 
     if len(checks) == 0:
-        print "There is no matching node!"
+        print("There is no matching node!")
         return 1
 
     if len(filtered) == 0:
-        print "There is no matching check!"
+        print("There is no matching check!")
         return 1
 
-    checkOutput = lambda x: x["Name"] + ":" + x["Output"]
-
     if len(critical):
-        print "|".join(map(checkOutput, critical))
+        print("|".join(map(check_output, critical)))
         for check in critical:
-            printCheck(check)
+            print_check(check)
     if len(warning):
-        print "|".join(map(checkOutput, warning))
+        print("|".join(map(check_output, warning)))
         for check in warning:
-            printCheck(check)
+            print_check(check)
     if len(passing):
-        print "Passing: %d" % (len(passing))
+        print("Passing: %d" % (len(passing)))
         for check in passing:
-            printCheck(check)
+            print_check(check)
 
     return 2 if len(critical) else 1 if len(warning) else 0
 
+
+def prepare_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', action='store_true', help='debug output')
+
+    subparsers = parser.add_subparsers(help='check individual node')
+    node_parser = subparsers.add_parser('node')
+    node_parser.add_argument('NODE', metavar='NODE', help='the consul node_name')
+    node_parser.add_argument('DC', metavar='DC', help='the consul datacenter')
+    node_parser.add_argument('--addr', default='http://localhost:8500',
+                             help='consul address [default: http://localhost:8500]')
+    node_parser.add_argument('--CheckID', default=None, type=str, help='CheckID matcher')
+    node_parser.add_argument('--ServiceName', default=None, type=str, help='ServiceName matcher')
+
+    return parser.parse_args().__dict__
+
+
 if __name__ == '__main__':
     try:
-        arguments = docopt(__doc__)
-        dump("Arguments: " + str(arguments))
-        if arguments['node']:
-            url = buildNodeUrl()
-            json = getJsonFromUrl(url)
-            exit(processFailing(json))
-    except exceptions.SystemExit: raise
-    except:
+        arguments = prepare_args()
+
+        url = build_node_url()
+        json_response = get_json_from_url(url)
+        exit(process_failing(json_response))
+    except SystemExit:
+        raise
+    except Exception:
         traceback.print_exc()
         exit(3)
